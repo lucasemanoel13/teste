46bbff6859406aeb19a7bfe2f037a43e1dee39cb
lucasemanoel13 <lucasemanoel689@gmail.com>
2024-11-30 19:03:12 -0300

    Add files via upload

---
+++ plugins/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $("#banners ul").bxSlider({
+        auto: true,
+        // slide vai passar automaticamente
+        speed: 1000,
+        // velocidade do slide
+        // mode: 'fade'
+        // o slide agora passa em "fade"
+        // pager: false
+
+        // biblioteca para slide das imagens
+    });
+});
